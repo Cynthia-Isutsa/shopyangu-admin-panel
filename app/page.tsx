a0de13ea7033b2ffd09089d4c9a6d546660cad4c
Cynthia-Isutsa <isutsacynthia@gmail.com>
2024-11-27 12:34:38 +0300

    feat: layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h2>Hello Cyn</h2>
+    <h2>Home Page</h2>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+
+import React from 'react'
+import { Nav } from './ui/nav'
+import {
+    AlertCircle,
+    Archive,
+    MessagesSquare,
+    ShoppingCart,
+    Users2,
+  } from "lucide-react"
+
+interface MailProps {
+    // accounts: {
+    //   label: string
+    //   email: string
+    //   icon: React.ReactNode
+    // }[]
+    // defaultLayout: number[] | undefined
+    defaultCollapsed?: boolean
+    // navCollapsedSize: number
+  }
+
+const Sidebar = ({
+    defaultCollapsed = false,
+  }: MailProps) => {
+    const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed)
+  return (
+    <div>
+        <Nav
+            isCollapsed={isCollapsed}
+            links={[
+              {
+                title: "Dashboard",
+                icon: Users2,
+                href: "/",
+                variant: "ghost",
+              },
+              {
+                title: "Shop Management",
+                href: "/shop",
+                icon: AlertCircle,
+                variant: "ghost",
+              },
+              {
+                title: "Product Management",
+                href: "/product",
+                icon: MessagesSquare,
+                variant: "ghost",
+              },
+              
+            ]}
+          />
+    </div>
+  )
+}
+
+export default Sidebar
